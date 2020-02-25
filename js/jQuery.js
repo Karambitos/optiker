9ab8623fcd31082cadb65788f9ffa68dcea3ff71
Karambitos <51115245+Karambitos@users.noreply.github.com>
2020-02-25 17:59:15 +0200

    1

---
+++ js/jQuery.js
@@ -0,0 +1,36 @@
+// owlCarousel
+// ============================================
+$(document).ready(function() {
+  $(".slide-one").owlCarousel({
+    nav: true,
+    loop:true,
+    autoplay:true,
+    autoplayTimeout:5000,
+    autoplayHoverPause:true,
+    responsive: {
+      0: {
+        items: 1
+      }
+    }
+  });
+
+  $(".slide-two").owlCarousel({
+    margin: 10,
+    nav: true,
+    loop:true,
+    autoplay:false,
+    responsive: {
+      0: {
+        items: 1
+      },
+      550: {
+        items: 2
+      },
+      900: {
+        items: 3
+      }
+    }
+  });
+});
+// ============================================
+ 
---
+++ js/script.js
@@ -0,0 +1,42 @@
+'use strict';
+
+document.addEventListener('DOMContentLoaded', () => {
+   
+        const BtBrillengläser = document.querySelector('.Brillengläser');
+        const DropDownBrillengläser = document.querySelector('.Brillengläser-nav__drop');
+        const BtBrillenfassungen = document.querySelector('.Brillenfassungen');
+        const DropDownBrillenfassungen = document.querySelector('.Brillenfassungen-nav__drop');
+        const BtKontaktlinsen = document.querySelector('.Kontaktlinsen');
+        const DropDownKontaktlinsen = document.querySelector('.Kontaktlinsen-nav__drop');
+
+        BtBrillengläser.addEventListener("mouseenter", (event) => {
+            event.preventDefault();
+            DropDownBrillengläser.classList.add("modal__show");
+         });
+
+        BtBrillengläser.addEventListener("mouseleave", (event) => {
+            event.preventDefault();
+            DropDownBrillengläser.classList.remove("modal__show");
+         });
+
+         BtBrillenfassungen.addEventListener("mouseenter", (event) => {
+            event.preventDefault();
+            DropDownBrillenfassungen.classList.add("modal__show");
+         });
+
+         BtBrillenfassungen.addEventListener("mouseleave", (event) => {
+            event.preventDefault();
+            DropDownBrillenfassungen.classList.remove("modal__show");
+         });
+
+         BtKontaktlinsen.addEventListener("mouseenter", (event) => {
+            event.preventDefault();
+            DropDownKontaktlinsen.classList.add("modal__show");
+         });
+
+         BtKontaktlinsen.addEventListener("mouseleave", (event) => {
+            event.preventDefault();
+            DropDownKontaktlinsen.classList.remove("modal__show");
+         });
+       
+});
